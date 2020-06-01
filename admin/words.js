da9387d625a226b9c7010d534af53917cffbd09d
om2804 <om2804@mail.ru>
2020-06-01 09:31:18 +0200

    add translation

--- admin/words.js
+++ admin/words.js
@@ -16,30 +16,87 @@ systemDictionary = {
     },
     'Devices': {
         'en': 'Devices',
-        'ru': 'Устройства'
+        'de': 'Geräte',
+        'ru': 'Устройства',
+        'pt': 'Aparelhos',
+        'nl': 'Apparaten',
+        'fr': 'Dispositifs',
+        'it': 'dispositivi',
+        'es': 'Dispositivos',
+        'pl': 'Urządzenia',
+        'zh-cn': '设备'
     },
     'ADB host': {
-        'en': 'ADB host',
-        'ru': 'ADB хост'        
-    },
+        "en": "ADB host",
+        "de": "ADB-Host",
+        "ru": "ADB хост",
+        "pt": "Host ADB",
+        "nl": "ADB-host",
+        "fr": "Hôte ADB",
+        "it": "Host ADB",
+        "es": "ADB host",
+        "pl": "Host ADB",
+        "zh-cn": "亚行托管人"       
+    },    
     'ADB port': {
-        'en': 'ADB port',
-        'ru': 'ADB порт'        
+        "en": "ADB port",
+        "de": "ADB-Port",
+        "ru": "ADB порт",
+        "pt": "Porta ADB",
+        "nl": "ADB-poort",
+        "fr": "Port ADB",
+        "it": "Porta ADB",
+        "es": "Puerto ADB",
+        "pl": "Port ADB",
+        "zh-cn": "亚行端口"
     },
-    'IP Address': {
-        'en': 'IP Address',
-        'ru': 'IP адрес'
+    "IP Address": {
+        "en": "IP Address",
+        "de": "IP Adresse",
+        "ru": "IP адрес",
+        "pt": "Endereço de IP",
+        "nl": "IP adres",
+        "fr": "Adresse IP",
+        "it": "Indirizzo IP",
+        "es": "Dirección IP",
+        "pl": "Adres IP",
+        "zh-cn": "IP地址"
     },
-    'Port': {
-        'en': 'Port',
-        'ru': 'Порт'
+    "Port": {
+        "en": "Port",
+        "de": "Hafen",
+        "ru": "Порт",
+        "pt": "Porta",
+        "nl": "Haven",
+        "fr": "Port",
+        "it": "Porta",
+        "es": "Puerto",
+        "pl": "Port",
+        "zh-cn": "港口"
     },
-    'Name': {
-        'en': 'Name',
-        'ru': 'Имя'
+    "Name": {
+        "en": "Name",
+        "de": "Name",
+        "ru": "Имя",
+        "pt": "Nome",
+        "nl": "Naam",
+        "fr": "Nom",
+        "it": "Nome",
+        "es": "Nombre",
+        "pl": "Imię",
+        "zh-cn": "名称"
     },
-    'Enabled': {
-        'en': 'Enabled',
-        'ru': 'Включено'
-    }    
+    "Enabled": {
+        "en": "Enabled",
+        "de": "aktiviert",
+        "ru": "Включено",
+        "pt": "ativado",
+        "nl": "Ingeschakeld",
+        "fr": "Activée",
+        "it": "Abilitato",
+        "es": "Habilitado",
+        "pl": "Włączone",
+        "zh-cn": "已启用"
+    }
+    
 };
